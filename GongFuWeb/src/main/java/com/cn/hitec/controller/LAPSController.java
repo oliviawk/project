06cd6d4ba73a0eaf647f9c1766fd9f105ac9159a
Edward <edwardcmh@live.com>
2017-12-15 16:04:00 +0800

    增加LAPS数据查询接口和数据访问类

--- GongFuWeb/src/main/java/com/cn/hitec/controller/LAPSController.java
+++ GongFuWeb/src/main/java/com/cn/hitec/controller/LAPSController.java
@@ -1,9 +1,16 @@
 package com.cn.hitec.controller;
 
+import com.cn.hitec.bean.EsQueryBean_web;
+import com.cn.hitec.service.BasicResource;
+import com.cn.hitec.service.LAPSService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Map;
 
@@ -18,6 +25,11 @@ import java.util.Map;
 public class LAPSController {
     private static final Logger logger = LoggerFactory.getLogger(LAPSController.class);
 
+    @Autowired
+    private LAPSService lapsService;
+    @Autowired
+    private BasicResource basicResource;
+
     @RequestMapping("/sxt")
     public String index() {
         // System.out.println("进入MICAPS4.html");
@@ -28,6 +40,18 @@ public class LAPSController {
     public String lct() {
         // System.out.println("进入MICAPS4.html");
         return "LAPS/produce";
+    }
+
+    /**
+     * LAPS数据查询
+     * @param esQueryBean
+     * @return
+     */
+    @RequestMapping(value="/getData", method = RequestMethod.POST, consumes = "application/json")
+    @ResponseBody
+    public Map getData(@RequestBody EsQueryBean_web esQueryBean){
+        Map<String,Object> map = lapsService.getData(esQueryBean);
+        return map;
     }
 
 
