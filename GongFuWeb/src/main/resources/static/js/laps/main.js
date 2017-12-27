1c8ca9126b57017b217ca8b79ef0acbe9266d51c
Edward <edwardcmh@live.com>
2017-12-27 17:49:48 +0800

    合并更新LAPS页面，加入LAPS分发中转状态展示

--- GongFuWeb/src/main/resources/static/js/laps/main.js
+++ GongFuWeb/src/main/resources/static/js/laps/main.js
@@ -7,27 +7,41 @@ $(function(){
     //console.log("test!");
 
     // TODO: IP暂时无过滤
+    // TODO: 合并查询，减少等待时间
     // 加工状态
     getLapsData('LapsTD', '加工', '10.30.16.224');
     sleep(100);
 
     getLapsData('LapsRain1Hour', '加工', '10.30.16.224');
-    sleep(100);
+    sleep(200);
 
     getLapsData('LapsWSWD', '加工', '10.30.16.224');
     sleep(100);
 
     getLapsData('LapsTRH', '加工', '10.30.16.224');
+    sleep(100);
 
     // 分发状态
-    /*
-    getLapsData('TD', '分发', '');
-    getLapsData('PRCPV', '分发', '');
-    getLapsData('EU4', '分发', '');
-    getLapsData('T', '分发', '');
-    getLapsData('RH', '分发', '');
-*/
+    // 中转和分发一起取
+    getLapsData('LAPS3KMGEO_PRCPV', '分发', '');
+    sleep(200);
 
+    getLapsData('LAPS3KMGEO_EU4', '分发', '');
+    sleep(100);
+
+    getLapsData('LAPS3KMGEO_TD', '分发', '');
+    sleep(200);
+
+    getLapsData('LAPS3KMGEO_T', '分发', '');
+    sleep(100);
+
+    getLapsData('LAPS3KMGEO_RH', '分发', '');
+    sleep(100);
+
+    getLapsData('LAPS3KM_ME', '分发', '');
+    //sleep(300);
+
+    //$("#Laps_分发").css("margin", "20px");
 
 });
 
@@ -78,7 +92,7 @@ function getLapsData(type, module, ip) {
         success: function (d) {
             //console.log(d);
             if (d.result == 'success') {
-                if (d.resultData.length > 1) {  // TODO: 最新时次判断条件
+                if (d.resultData.length > 1) {  // TODO: 最新时次未处理的判断条件
                     // 有数据
                     var data = d.resultData;
                     dataRecv.proc[data[0].type] = data;
@@ -86,15 +100,27 @@ function getLapsData(type, module, ip) {
                     // 设置列表状态
                     var subType = data[0].type;
                     var module = data[0].fields.module;
-                    /*
-                    var moduleEng = "";
+
                     if (module == "加工") {
-                        moduleEng = "process";
+                        $("#" + subType + "_" + module).attr("class", "list-green");
+                        $("#" + subType + "_" + module).attr("title", data[0].fields.data_time);
+
                     } else if (module == "分发") {
-                        moduleEng = "distribute";
+                        // data[0]为外网分发data[1]为内网分发
+                        // 分发id编码type_module_ip末位
+                        var suff_1 = data[0].fields.ip_addr.split('.')[3];
+                        var suff_2 = data[1].fields.ip_addr.split('.')[3];
+                        //console.log(suff_1 + "-" + suff_2);
+                        var selecter_1 = "#" + subType + "_" + module + "_" + suff_1;
+                        var selecter_2 = "#" + subType + "_" + module + "_" + suff_2;
+                        var time_1 = data[0].fields.data_time;
+                        var time_2 = data[1].fields.data_time;
+                        $(selecter_1).attr("class", "list-green");
+                        $(selecter_2).attr("class", "list-green");
+                        $(selecter_1).attr("title", time_1);
+                        $(selecter_2).attr("title", time_2);
+
                     }
-                    */
-                    $("#" + subType + "_" + module).attr("class", "list-green");
 
 
                     // 判断数据是否准备好
