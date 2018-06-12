c3ea34d3d1ed0f61ed9ac1e680fef88074d6f658
caoyz <1679344354@qq.com>
2018-06-12 18:18:04 +0800

    1.DateTool工具类增加获取指定时间差的方法getDateTimeFromNow() 2.BasicResource类增加获取基础资源告警信息的方法getEventData() 3.LAPS和FZJC增加/getBaseEventData接口 4.fzjc的fzjc.js和laps的main.js增加获取基础资源告警信息并更新页面的函数

--- GongFuWeb/src/main/resources/static/js/fzjc/fzjc.js
+++ GongFuWeb/src/main/resources/static/js/fzjc/fzjc.js
@@ -40,3 +40,52 @@ $('#baseSourceModal').on('shown.bs.modal', function (event) {
 
 
 })
+
+$(document).ready(function () {
+    //一分钟自动刷新一次
+    setInterval(getBase, 60000);
+
+    function getBase() {
+        var url = "../fzjc/getBaseEventData";
+        var params = {
+            "listIp": [
+                "10.30.16.223",
+                "10.30.16.220",
+                "10.0.74.226"
+            ],
+            "minute": -240
+        }
+        $.ajax({
+            type: 'POST',
+            url: url,
+            data: JSON.stringify(params),
+            dataType: "json",
+            headers: {
+                "Content-Type": "application/json; charset=utf-8"
+            },
+            success: function (data2) {
+                if ("fail" == data2["result"]) {
+                    console.log("获取基础设施告警信息数据失败,失败原因："
+                        + data2["message"]);
+                } else {
+                    var resultdata = data2["resultData"];
+                    for (var i in resultdata) {
+                        var ary = resultdata[i];
+                        var div_ip = $('div[data-ip="' + i + '"]');
+                        if (div_ip.length > 0) {
+                            div_ip.find('li').removeClass();
+                            for (var j in ary) {
+                                var num = ary[j];
+                                var class_name = num == 0 ? "green" : "red";
+                                div_ip.find('li').eq(j).addClass(class_name);
+                            }
+                        }
+                    }
+                }
+            },
+            error: function (e2) {
+                console.error(e2)
+            }
+        });
+    }
+})
