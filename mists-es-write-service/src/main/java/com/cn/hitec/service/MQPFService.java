69feaed040701c6dd698a97b81afbd0229571049
fukeli <www.fukeli@qq.com>
2018-07-11 17:01:26 +0800

    mqpf采集数据入库

---
+++ mists-es-write-service/src/main/java/com/cn/hitec/service/MQPFService.java
@@ -0,0 +1,105 @@
+package com.cn.hitec.service;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.cn.hitec.bean.AlertBeanNew;
+import com.cn.hitec.repository.ESRepository;
+import com.cn.hitec.repository.jpa.DataInfoRepository;
+import com.cn.hitec.tools.AlertType;
+import com.cn.hitec.tools.Pub;
+import org.elasticsearch.action.delete.DeleteResponse;
+import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.common.xcontent.XContentType;
+import org.elasticsearch.rest.RestStatus;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class MQPFService {
+    private static final Logger logger = LoggerFactory.getLogger(MQPFService.class);
+    @Autowired
+    private ESRepository es;
+
+
+    /**
+     * MQPF业务 雷达采集日志入库
+     * @param listJson
+     * @return
+     */
+    public int insertMQPFCollectionData(List<String> listJson){
+        int error_num = 0;
+        int listSize = 0;
+        try {
+            if (listJson == null || listJson.size() < 1) {
+                logger.error("参数为空");
+                return 0;
+            }
+            listSize = listJson.size();
+            Map<String, Object> map = null;
+            Map<String, Object> fields = null;
+            String index = "";
+            for (String json : listJson) {
+                try {
+                    if (StringUtils.isEmpty(json)) {
+                        logger.error("数据为空");
+                        error_num++;
+                        continue;
+
+                    }
+                    //给关键变量赋值
+                    map = JSON.parseObject(json);
+                    fields = (Map<String, Object>) map.get("fields");
+
+                    String subType = map.get("type").toString(); // 数据名称
+                    String subModule = fields.get("module").toString();
+                    String subIp = fields.get("ip_addr").toString();
+                    String subKey = "MQPF"+","+subType+","+subModule+","+subIp;
+                    String str_id = Pub.MD5(subKey + "," + fields.get("data_time"));
+
+                    Date dataTimeIndex = Pub.transform_StringToDate(fields.get("data_time").toString(),
+                            "yyyy-MM-dd HH:mm:ss.SSSZ");
+                    index = Pub.Index_Head + Pub.transform_DateToString(dataTimeIndex, Pub.Index_Food_Simpledataformat);
+
+
+                    // 如果是定时 有规律的数据， 需要查询后入库
+                    map.put("aging_status", "正常");
+                    if(fields.containsKey("event_status")){
+                        // 判断数据状态
+                        if (!fields.get("event_status").toString().toUpperCase().equals("OK")
+                                && !fields.get("event_status").toString().equals("0")) {
+                            map.put("aging_status", "异常");
+                        }
+                    }
+
+                    // 数据入库
+                    es.bulkProcessor.add(new IndexRequest(index, "MQPF", str_id).source(map));
+//                    logger.info(JSON.toJSONString(map));
+//						DIMap = null;
+
+                    //开始预生成接下来加工、分发的数据
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    logger.error("mqpf 错误数据："+json);
+                    error_num++;
+                }
+            }
+            System.out.println("mqpf---------------------------------------------------------------------------------------------------------");
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            return listSize - error_num;
+        }
+
+    }
+}
