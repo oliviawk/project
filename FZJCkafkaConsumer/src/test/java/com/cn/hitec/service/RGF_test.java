a46bf7a3908ee755fa98b8c268804c2932e266b2
981038472@qq.com <pmsc123>
2018-08-02 15:11:45 +0800

    RGF入库

--- FZJCkafkaConsumer/src/test/java/com/cn/hitec/service/RGF_test.java
+++ FZJCkafkaConsumer/src/test/java/com/cn/hitec/service/RGF_test.java
@@ -1,31 +1,37 @@
 package com.cn.hitec.service;
 
 
-import com.alibaba.fastjson.JSON;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.*;
 
-@Service
-public class RGF_Consumer extends MsgConsumer{
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ActiveProfiles("dev")
+public class RGF_test {
 
-    private static final Logger logger = LoggerFactory.getLogger(RGF_Consumer.class);
-    private static String topic = "RGF";
-    private static String type = "RGF";
+    @Autowired
+    RGF_Consumer rgf_consumer;
 
-    private static String INFO = "!!!Info";
+    @Test
+    public void test1(){
 
-    public RGF_Consumer(@Value("${RGF.group.id}")String group) {
-        super(topic, group, type);
+        rgf_consumer.consume();
+
     }
 
-    @Override
-    public List<String> processing(String msg) throws ParseException {
+    @Test
+    public  void  test2() throws ParseException {
+
+        String msg = "10.30.16.220※log_tran_eleh_20180802.log※!!!Info:proc tran ftp://10.0.74.170/eleh/ at 10:53:18......※MSP1_PMSC_ELEH_ME_L88_CHN_201808020230_00030-00000.nc transfer completed with status 0. Total time:11.581553 sec. File size:129105920 bytes. File mtime:10:53:18.※!!!Info:proc tran ftp://10.14.83.137/eleh/ at 10:53:29......※MSP1_PMSC_ELEH_ME_L88_CHN_201808020230_00030-00000.nc transfer completed with status 0. Total time:1.161015 sec. File size:129105920 bytes. File mtime:10:53:18.※!!!Info:proc tran ftp://10.0.122.172/ser/grid/nc/obs/10m/ at 10:53:31......※10.30.16.220※log_tran_eleh_20180802.log※!!!Info:proc tran ftp://10.0.74.170/eleh/ at 10:53:32......※MSP1_PMSC_ELEH_ME_L88_CHN_201808020230_00030-00000.nc transfer completed with status 0. Total time:1.380464 sec. File size:129105920 bytes. File mtime:10:53:18.※###############################  process end at 10:53:32  ###############################※";
+//        String msg = "10.30.16.220※log_tran_h8_nc_20180802.log※!!!Info:proc tran ftp://10.14.83.137/h8_nc/ at 11:20:18......※10.30.16.220※log_tran_h8_nc_20180802.log※!!!Info:proc tran ftp://10.14.83.137/h8_nc/ at 11:20:33......※10.30.16.220※log_tran_h8_nc_20180802.log※!!!Info:proc tran ftp://10.14.83.137/h8_nc/ at 11:20:48......※HS_H08_20180802_0250_B01_FLDK_R10_S0110.nc transfer completed with status 0. Total time:30.969668 sec. File size:83361792 bytes. File mtime:11:20:18.※!!!Info:proc tran ftp://10.0.74.170///home/docker/TDS/data/h8_nc/ at 11:20:52......※HS_H08_20180802_0250_B01_FLDK_R10_S0110.nc transfer completed with status 21. Total time:18.944589 sec. File size:98548430 bytes. File mtime:11:20:21.※curl_error message:QUOT string not accepted: RNFR HS_H08_20180802_0250_B01_FLDK_R10_S0110.nc.tmp※!!!Info:proc tran ftp://10.0.74.170///home/docker/TDS/data/h8_nc/ at 11:20:54......※HS_H08_20180802_0250_B01_FLDK_R10_S0110.nc transfer completed with status 0. Total time:1.843645 sec. File size:83361792 bytes. File mtime:11:20:18.※###############################  process end at 11:20:55  ###############################※";
 
         List<String> list = new ArrayList<>();
 
@@ -37,7 +43,7 @@ public class RGF_Consumer extends MsgConsumer{
             String strName = fileNames[3].replace(".log","");
             Date occTimeD = null;
             for(int i=0;i < lines.length;i++){
-                if(lines[i].indexOf("MSP1_") > -1 && lines[i].indexOf("mtime") > -1){
+                if(lines[i].indexOf("MSP1_") > -1 ||lines[i].indexOf("mtime") > -1){
                     HashMap<String,Object> file =new HashMap();
                     HashMap map = new HashMap();
                     String[] line = lines[i].split( " ");
@@ -72,7 +78,6 @@ public class RGF_Consumer extends MsgConsumer{
                     map.put("occur_time",occTimeD.getTime());
                     map.put("fields",file);
 
-                    list.add(JSON.toJSONString(map));
                     System.out.println(map.toString());
 
                 }
@@ -113,13 +118,11 @@ public class RGF_Consumer extends MsgConsumer{
                     map.put("occur_time",occTimeD.getTime());
                     map.put("fields",file);
 
-                    list.add(JSON.toJSONString(map));
                     System.out.println(map.toString());
 
                 }
             }
         }
-        return list;
-    }
 
+    }
 }
